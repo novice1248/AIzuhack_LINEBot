1e260f6a0486ec4e8f305788bc6d89d79e17ab26
novice <shirodora1248@gmail.com>
2023-07-12 19:31:52 +0900

    時間を取得してresを返す

--- src/express/server.js
+++ src/express/server.js
@@ -1,6 +1,5 @@
 // モジュールの読み込み
 import express from 'express';
-import { middleware } from '@line/bot-sdk';
 import 'dotenv/config';
 
 // ファイルの読み込み
@@ -13,10 +12,31 @@ const app = express();
 
 // /にアクセスがあった時、Deploy succeededと返す
 app.get('/', (_req, res) => { res.send('Deploy succeeded'); });
+
+// /time-open にアクセスがあった時、日時を取得して7時0~3分の時 succeededと返す
+app.get('/time-open',(_req, res) => {
+  const hour = new Date().getHours();
+  const minute = new Date().getMinutes();
+  if(hour === 7 && minute < 3) {
+    res.send('succeeded');
+  } else {
+    res.send('Error\n');
+  }
+});
+
+// /time-close にアクセスがあった時、日時を取得して8時0~3分の時 succeededと返す
+app.get('/time-close',(_req, res) => {
+  const hour = new Date().getHours();
+  const minute = new Date().getMinutes();
+  if(hour === 8 && minute < 3) {
+    res.send('succeeded');
+  } else {
+    res.send('Error\n');
+  }
+});
+
 // /webhookにアクセスがあったとき、bot.jsのindexを呼び出す
-app.post('/webhook', middleware({
-  channelSecret: CHANNEL_SECRET,
-}), webhook);
+app.post('/webhook', webhook);
 
 app.listen(PORT);
 log(`Server running at ${PORT}`);
