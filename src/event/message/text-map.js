e4f9c2093994c85403997fce58c07e4bdcb83264
novice <shirodora1248@gmail.com>
2023-07-14 00:09:25 +0900

    Hand-on-linebotにかかれているdbのすべてを入力

--- src/event/message/text-map.js
+++ src/event/message/text-map.js
@@ -1,3 +1,7 @@
+import {
+  createData, deleteData, readData, updateData,
+} from '../../crud.js';
+
 // ユーザーのプロフィールを取得する関数
 const getUserProfile = (event, client) => client.getProfile(event.source.userId);
 
@@ -27,4 +31,55 @@ export const messageMap = {
     type: 'text',
     text: 'Hello, world',
   }),
+  Create: async (event, appContext) => {
+    const date = new Date();
+    await createData(event.source.userId, 'testData', `Data created at ${date}`, appContext);
+    return {
+      type: 'text',
+      text: 'データが作成されました',
+    };
+  },
+  Read: async (event, appContext) => {
+    const dbData = await readData(event.source.userId, 'testData', appContext);
+    return {
+      type: 'text',
+      text: `DBには以下のデータが保存されています\n\n${dbData.Items[0].Data}`,
+    };
+  },
+  Update: async (event, appContext) => {
+    const date = new Date();
+    await updateData(event.source.userId, 'testData', `Data created at ${date}`, appContext);
+    return {
+      type: 'text',
+      text: 'データを更新しました',
+    };
+  },
+  Delete: async (event, appContext) => {
+    await deleteData(event.source.userId, 'testData', appContext);
+    return {
+      type: 'text',
+      text: 'データを削除しました',
+    };
+  },
+  メモ: async (event, appContext) => {
+    const memoData = await readData(event.source.userId, 'memo', appContext);
+    if (memoData.Items[0]) {
+      return {
+        type: 'text',
+        text: `メモには以下のメッセージが保存されています\n\n${memoData.Items[0].Data}`,
+      };
+    }
+
+    return {
+      type: 'text',
+      text: 'メモが存在しません',
+    };
+  },
+  メモ開始: async (event, appContext) => {
+    await createData(event.source.userId, 'context', 'memoMode', appContext);
+    return {
+      type: 'text',
+      text: 'メモモードを開始しました',
+    };
+  },
 };
